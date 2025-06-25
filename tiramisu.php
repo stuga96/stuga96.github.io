27ae0acfd63e88063249c27cf0a9f1a49165302c
stuga96 <ola.andersson@su.se>
2025-06-25 23:59:00 +0200

    tjoho

---
+++ tiramisu.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<meta name="robots" content="noindex,nofollow" />
+		<meta charset="utf-8" />
+		<title>Hallontiramisu</title>
+		<link rel="stylesheet" type="text/css" href="css/photon.css" />
+		<link href="http://fonts.googleapis.com/css?family=Geo" rel="stylesheet" type="text/css">
+		<link rel="shortcut icon" href="img/favicon.ico">
+	</head>
+	<body>
+		<div class="main">
+			<div class="centered">
+				<h2>Ingredienser</h2>
+				<ul>
+					<li>125 g mascarpone</li>
+					<li>1 dl grädde</li>
+					<li>50 g florsocker</li>
+					<li>0,5 tsk vaniljpulver</li>
+					<li>100 g färska hallon</li>
+					<li>100 g biscoffkakor</li>
+					<li>50 ml hallonjuice</li>
+					<li>lite kakao</li>
+				</ul>
+				<ol>
+				<li>Blanda mascarpone med florsocker och vaniljpulver i en stor skål tills det är slätt.</li>
+				<li>Vispa vispgrädden i en separat skål tills det bildas styva toppar och vänd sedan försiktigt ner den i mascarponeblandningen.
+				<li>Häll hallonjuicen i en grund form. Doppa snabbt biscoffkakorna i den så att de är lätt blötlagda men inte för mjuka.
+				<li>Varva de blötlagda biscoffkakorna i en ugnsform. Bred ett lager mascarponekräm ovanpå och lägg på ett lager hallon.
+				<li>Upprepa processen tills alla ingredienser är använda, se till att det sista lagret är mascarponekräm.
+				<li>Låt tiramisu kallna i kylskåpet i minst 4 timmar, helst över natten.
+				<li>Pudra med kakaopulver och garnera med färska hallon före servering.
+					</ol>
+			</div><!-- centered -->
+			<div class="maintext">
+			</div>
+			</div><!-- centered -->
+		</div><!-- main -->
+	</body>
+</html>
